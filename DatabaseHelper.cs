f8aac8d1bae1f8ceebe5067ebdac1be8eb7a0216
punochka <patnicapatnica551@gmail.com>
2026-04-26 02:24:58 +1000

    добавила класс DataBaseHelper

---
+++ DatabaseHelper.cs
@@ -0,0 +1,43 @@
+﻿using Npgsql;
+using System.Data;
+
+namespace Kurs
+{
+    public static class DatabaseHelper
+    {
+        private static string connectionString = "Server=localhost;Port=5432;User ID=postgres;Password=3455;Database=Kurs;";
+
+        public static DataTable ExecuteQuery(string query, NpgsqlParameter[] parameters = null)
+        {
+            using (var conn = new NpgsqlConnection(connectionString))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand(query, conn))
+                {
+                    if (parameters != null)
+                        cmd.Parameters.AddRange(parameters);
+                    using (var adapter = new NpgsqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+                        return dt;
+                    }
+                }
+            }
+        }
+
+        public static int ExecuteNonQuery(string query, NpgsqlParameter[] parameters = null)
+        {
+            using (var conn = new NpgsqlConnection(connectionString))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand(query, conn))
+                {
+                    if (parameters != null)
+                        cmd.Parameters.AddRange(parameters);
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
